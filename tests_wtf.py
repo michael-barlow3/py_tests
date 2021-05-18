e1a317bf68b314615bcf4a3a1ba2d5779d1dd284
Mike Barlow <michael.barlow3@va.gov>
2021-05-18 15:51:23 -0400

    more3

--- tests_wtf.py
+++ tests_wtf.py
@@ -31,37 +31,37 @@ def test_multi_def_payload(client):
     assert b' - Abdominal Aortic Aneurysm;' in r.data
     assert b' - Area Agencies on Aging;' in r.data
 
-def test_context_payload(client):
-    data = {'text': '3pao','token': TEST_TOKENS[0]}
-    r = client.post(ROUTE, data=data)
-    # print (r.data)
-    assert b'3pao\n - Third Party Assessment Organization\n\t- FedRAMP requires a 3PA0 to verify the attestations made in an SSP' in r.data
+# def test_context_payload(client):
+#     data = {'text': '3pao','token': TEST_TOKENS[0]}
+#     r = client.post(ROUTE, data=data)
+#     # print (r.data)
+#     assert b'3pao\n - Third Party Assessment Organization\n\t- FedRAMP requires a 3PA0 to verify the attestations made in an SSP' in r.data
 
-def test_note_payload(client):
-    data = {'text': 'ahlta','token': TEST_TOKENS[0]}
-    r = client.post(ROUTE, data=data)
-    assert b'ahlta\n - Armed Forces Health Longitudinal Technology Application\n\t- DoD electronic medical record system' in r.data    
+# def test_note_payload(client):
+#     data = {'text': 'ahlta','token': TEST_TOKENS[0]}
+#     r = client.post(ROUTE, data=data)
+#     assert b'ahlta\n - Armed Forces Health Longitudinal Technology Application\n\t- DoD electronic medical record system' in r.data    
 
-def test_comma_in_def(client):
-    data = {'text': 'acre','token': TEST_TOKENS[0]}
-    r = client.post(ROUTE, data=data)
-    assert b'A measure of land 43,560 sq. ft.' in r.data
+# def test_comma_in_def(client):
+#     data = {'text': 'acre','token': TEST_TOKENS[0]}
+#     r = client.post(ROUTE, data=data)
+#     assert b'A measure of land 43,560 sq. ft.' in r.data
 
-def test_bad_payload(client):
-    data = {'foo': 'bar', 'token': TEST_TOKENS[0]}
-    r = client.post(ROUTE, data=data)
-    assert b'Improper request' in r.data
-    assert r.status_code == http.HTTPStatus.BAD_REQUEST
+# def test_bad_payload(client):
+#     data = {'foo': 'bar', 'token': TEST_TOKENS[0]}
+#     r = client.post(ROUTE, data=data)
+#     assert b'Improper request' in r.data
+#     assert r.status_code == http.HTTPStatus.BAD_REQUEST
 
-def test_no_slack_token(client):
-    data = {'text': 'vba','token': 'foobar'}
-    r = client.post(ROUTE, data=data)
-    assert b'Not authorized' in r.data
-    assert r.status_code == http.HTTPStatus.UNAUTHORIZED
+# def test_no_slack_token(client):
+#     data = {'text': 'vba','token': 'foobar'}
+#     r = client.post(ROUTE, data=data)
+#     assert b'Not authorized' in r.data
+#     assert r.status_code == http.HTTPStatus.UNAUTHORIZED
 
-def test_not_found(client):
-    data = {'text': '13231312334','token': TEST_TOKENS[0]}
-    r = client.post(ROUTE, data=data)
-    assert b'not found!' in r.data
-    assert b'13231312334' in r.data
-    assert r.status_code == http.HTTPStatus.OK
+# def test_not_found(client):
+#     data = {'text': '13231312334','token': TEST_TOKENS[0]}
+#     r = client.post(ROUTE, data=data)
+#     assert b'not found!' in r.data
+#     assert b'13231312334' in r.data
+#     assert r.status_code == http.HTTPStatus.OK
